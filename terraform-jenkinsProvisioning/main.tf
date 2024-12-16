39bb8b82b1cd8450fb9a179924fcdd1bf93b29c3
LOKEN001 <lokendraprajapati354@gmail.com>
2024-12-16 15:32:35 +0530

    jenkins provisioned

--- terraform-jenkinsProvisioning/main.tf
+++ terraform-jenkinsProvisioning/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    jenkins = "J-vpc"
+    jenkins     = "J-vpc"
     Terraform   = "true"
     Environment = "dev"
   }
@@ -24,30 +24,36 @@ module "vpc" {
 module "vote_service_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "Jenkis-SG"
-  vpc_id      = module.vpc.vpc_id       #fatching vpc id from vpc module
+  name   = "Jenkis-SG"
+  vpc_id = module.vpc.vpc_id #fatching vpc id from vpc module
 
   ingress_with_cidr_blocks = [
     {
-      from_port   = 8080
-      to_port     = 8080
-      protocol    = "HTTP"
-      cidr_blocks = "0.0.0.0/0"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks =  "0.0.0.0/0"
     },
-     {
+    {
       from_port   = 22
       to_port     = 22
-      protocol    = "SSH"
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
 
   egress_with_cidr_blocks = [
     {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = "0.0.0.0/0"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks =  "0.0.0.0/0"
     }
   ]
 
@@ -62,16 +68,20 @@ module "vote_service_sg" {
 
 
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "jenkins-master"
 
-  instance_type          = var.instance_type
-  key_name               = "terra-key"
-  monitoring             = true
-  vpc_security_group_ids = module.vote_service_sg.security_group_id
-  subnet_id              = module.vpc.public_subnets[0]
+  instance_type               = var.instance_type
+  key_name                    = "terra-key"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.vote_service_sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
+
+  user_data = file("toolsinstallation.sh")
+
+  availability_zone = data.aws_availability_zones.azs.names[0]
 
   tags = {
     Terraform   = "true"
--- terraform-jenkinsProvisioning/veriable.tf
+++ terraform-jenkinsProvisioning/veriable.tf
@@ -7,3 +7,8 @@ variable "subnet_cidr" {
   description = "Subnet CIDR"
   type        = list(string)
 }
+
+variable "instance_type" {
+  description = "EC2 instance"
+  type        = string
+}
